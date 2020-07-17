f2915755294ffe765d8ba5c0d7ddb1f3bdbea2ee
Abdullah <abdullah.mohammed-int@dubizzle.com>
2020-07-17 12:13:06 +0400

    styled-components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-import Head from "next/head";
+import React from 'react';
+import styled from 'styled-components';
 
-export default function Home() {
-  return (
-    <h1>hello world</h1>
-  )
-}
+
+export default () => <Title>Mastering Next.js</Title>;
+
+const Title = styled.h1`
+    font-size: 50px;
+    color: ${({theme}) => theme.colors.primary};
+`;
